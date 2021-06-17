f4af9b54191b4be0f54db0437e76f1bae4d160f9
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-06-17 13:46:55 +0800

    Add files via upload

--- visual_module/src/singlePt_manipulate_main.cpp
+++ visual_module/src/singlePt_manipulate_main.cpp
@@ -1,17 +1,23 @@
 #include "ros/ros.h"
+#include <eigen3/Eigen/QR>
 #include "arm_include/FvrRobotClient.hpp"
 #include "arm_include/ConnectionManager.hpp"
 #include "arm_include/ClientTestFunction.hpp"
-
 #include "visual_module/visual_info_service_singlePt.h"
 
 using namespace fvr;
 
+int ONLINE_MOVE_MODE = 8;
+float motion_amplitude = 0.01;
+
 Eigen::Matrix3f camera2base;
 Eigen::Vector2f feedback_pt, target_pt,
                 ee_pt, DO_to_obs_DO_pt, DO_to_obs_obs_pt,
-                projection_on_path_pt;
-Eigen::Matrix2f Jd;
+                projection_on_path_pt,
+                error_pt, ee_velocity_image;
+Eigen::Vector3f ee_velocity_image_3D = Eigen::Array3f::Zero(),
+                ee_velocity_3D;
+Eigen::Matrix2f Jd = Eigen::Matrix2f::Identity();
 
 void ProcessServece(const visual_module::visual_info_service_singlePt& srv) {
     feedback_pt(0, 0) = srv.response.feedback_pt[0];
@@ -33,20 +39,38 @@ void ProcessServece(const visual_module::visual_info_service_singlePt& srv) {
     DO_to_obs_obs_pt(1, 0) = srv.response.DO_to_obs_obs_pt[1];
     projection_on_path_pt(0, 0) = srv.response.projection_on_path_pt[0];
     projection_on_path_pt(1, 0) = srv.response.projection_on_path_pt[1];
+    
+    error_pt = projection_on_path_pt - target_pt;
+    ee_velocity_image = -Jd.inverse() * error_pt;
+    ee_velocity_image_3D(0, 0) = ee_velocity_image(0, 0);
+    ee_velocity_image_3D(1, 0) = ee_velocity_image(1, 0);
+
+    std::cout << "Jd: \n" << Jd 
+            << "\nJd^-1:\n" << Jd.inverse() << '\n';
 }
 
 
 int main(int argc, char** argv) {
     camera2base <<  1, 0, 0,
                     0, 1, 0,
-                    0, 0, 1;     
+                    0, 0, 1;
     std::shared_ptr<FvrRobotClient> robot = std::make_shared<FvrRobotClient>();
     
     const std::string server_address = "1";
     const std::string client_address = "2";
     ConnectionManager robot_connection(robot, server_address, client_address);
-    
-    ros::init(argc, argv, "service_client_test");
+    std::thread connection([&]() {
+        while (true) {
+            if (robot_connection.run() != true)
+                return
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+
+    ClientTestFunction client_tester(robot);
+    // client_tester.printTestCase();
+
+    ros::init(argc, argv, "manipulate_singelePt_main");
     ros::NodeHandle node_handle;
     ros::ServiceClient service_client = node_handle.serviceClient<visual_module::visual_info_service_singlePt>("visual_info_service_singlePt");
     visual_module::visual_info_service_singlePt srv;
@@ -66,6 +90,18 @@ int main(int argc, char** argv) {
         }
 
         ProcessServece(srv);
+        ee_velocity_3D = camera2base * ee_velocity_image_3D;
+        ee_velocity_3D = motion_amplitude / ee_velocity_3D.norm() * ee_velocity_3D;
+
+        if (robot_connection.robotConnected() == false) {
+            std::cout << "Fial to connect to robot server!\n";
+            continue;
+        }
+        if (client_tester(ONLINE_MOVE_MODE, ee_velocity_3D) != true) {
+            std::cout << "Fail to execute the command!\n";
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
     return 0;
 }
